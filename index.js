d8cb758d6de6b94003e62f4bc544999b43dd7d31
prateeksrivastav2 <prateeksrivastav703@gmail.com>
2024-02-23 22:14:08 +0530

    first commit

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ app.use(
   })
   );
   app.use(Cors());
-
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 const __dirnam = path.dirname(__filename);
 const storage = multer.diskStorage({
